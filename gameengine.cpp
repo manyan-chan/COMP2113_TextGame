c0f857b4156a9258e5f9e5e99f1f1b433991d53c
yaoronald <61876269+yaoronald@users.noreply.github.com>
2020-05-07 15:19:13 +0800

    changed array to vector

--- gameengine.cpp
+++ gameengine.cpp
@@ -10,7 +10,7 @@ struct location_array{
   bool mc;
 };
 
-void open_location_info(int resource_list[], int resource_amount[], int arraySize){
+void open_location_info(int resource_list[], int resource_amount[], int arraySize, string location, location_array location_database[], int location_array_size, int load_pos){
 
   //creating the array
 
@@ -24,6 +24,14 @@ void open_location_info(int resource_list[], int resource_amount[], int arraySiz
     cout << line;
   }
   fin.close();
+
+  //check this is mc or not
+  bool check;
+  for (int i = 0; i < location_array_size; i++){
+    if (location_database[i].location == location && location_database[i].visit_time == load_pos){ //find the corresponding element in the array
+      check = location_database[i].mc; //true or false only
+    }
+  }
 
   //user is going to input his choice
   char choice;
@@ -50,6 +58,7 @@ void game_engine()
   string location;
 
   //--------------------- load location database, can put in separate file ------------------------
+  const int location_array_size;
   location_array location_database[27];
   ifstream loc_master;
   loc_master.open("plot/location_master.txt");
@@ -62,42 +71,44 @@ void game_engine()
     fin >> location_database[i].mc;
   }
   //--------------------- load location database, can put in separate file ------------------------
-
+  int location_count [9] = {1, 1, 1, 1, 1, 1, 1, 1, 1}; //between 1-3, indicated the coming event is the n-th visit time
+  //{"supermarket", "hospital", "coffeeshop", "convenientstore", "schoolhall", "chemistrylaboratory", "lawn", "communitymarket"};
   switch (cases)
   {
     case ('A'):
         location = "supermarket";
-        open_location_info(resource_list, resource_amount, arraySize, location);
+        load_pos = location_count[0]；
         break;
     case ('B'):
-        location = "laboratory";
-        laboratory();
+        location = "hospital";
+        load_pos = location_count[1]；
         break;
     case ('C'):
         location = "coffeeshop";
-        coffeeshop();
+        load_pos = location_count[2]；
         break;
     case ('D'):
         location = "convenientstore";
-        convenientstore();
+        load_pos = location_count[3]；
         break;
     case ('E'):
         location = "schoolhall";
-        schoolhall();
+        load_pos = location_count[4]；
         break;
     case ('F'):
         location = "chemistrylaboratory";
-        chemistrylaboratory();
+        load_pos = location_count[5]；
         break;
     case ('G'):
         location = "lawn";
-        lawn();
+        load_pos = location_count[6]；
         break;
     case ('H'):
         location = "communitymarket";
-        communitymarket();
+        load_pos = location_count[7]；
         break;
   }
+  open_location_info(resource_list, resource_amount, resource_arraySize, location, location_database, location_array_size, load_pos); //print location info
   day_count++;
 
   if (daycount == 7)
--- main.cpp
+++ main.cpp
@@ -6,7 +6,23 @@ int day_count = 0;
 
 int main()
 {
+    //-------------------------------------------------------------//
+
     display_background();
+
+    //-------------------------------------------------------------//
+
+    vector<string> resource_list = {"Food", "Water", "Mask", "Utility knife", "Realistic toy gun", "Clothes", "Radio", "Notebook & Pen", "Bible"};
+    vector<int> resource_amount = {0, 0, 0, 0, 0, 0, 0, 0, 0};
+    vector<int> resource_upperbound = {10, 10, 10, 2, 1, 5, 1, 1, 1};
+    int resource_vector_size = resource_list.size();
+    allocate_resource (resource_list, resource_amount, resource_upperbound, resource_vector_size);
+
+    //-------------------------------------------------------------//
+
     game_engine();
+
+    //-------------------------------------------------------------//
+
     return 0;
 }
